540ea28b053104cd24c7ea583dc36e6ab052a3f9
Sumedha <pramods@umich.edu>
2014-10-07 23:51:33 -0400

    Working on randomly dropping asteroids at dino

--- Assets/scripts/AstroidGenerator.cs
+++ Assets/scripts/AstroidGenerator.cs
@@ -5,7 +5,7 @@ public class AstroidGenerator : MonoBehaviour
 {
 		// fields set in the Unity Inspector pane
 		public int numAsteroids = 40;
-		public GameObject[] asteroidPrefabs;
+		public GameObject asteroidPrefab;
 		public Vector3 asteroidPosMin;
 		public Vector3 asteroidPosMax;
 		public float asteroidScaleMin = 1;
@@ -15,41 +15,40 @@ public class AstroidGenerator : MonoBehaviour
 		public bool ______________________________;
 	
 		// fields set dynamically
-		public GameObject[] cloudInstances;
+		public GameObject[] asteroidInstances;
 	
 		void Awake ()
 		{
-				cloudInstances = new GameObject[numClouds];
+				asteroidInstances = new GameObject[numAsteroids];
 				GameObject anchor = GameObject.Find ("AstroidAnchor");
-				GameObject cloud;
-				for (int i = 0; i < numClouds; i++) {
-						int prefabNum = Random.Range (0, cloudPrefabs.Length - 1);
-						cloud = Instantiate (cloudPrefabs [prefabNum]) as GameObject;
+				GameObject asteroid;
+				for (int i = 0; i < numAsteroids; i++) {
+						asteroid = Instantiate (asteroidPrefab) as GameObject;
 						Vector3 cPos = Vector3.zero;
-						cPos.x = Random.Range (cloudPosMin.x, cloudPosMax.x);
-						cPos.y = Random.Range (cloudPosMin.y, cloudPosMax.y);
+						cPos.x = Random.Range (asteroidPosMin.x, asteroidPosMax.x);
+						cPos.y = Random.Range (asteroidPosMin.y, asteroidPosMax.y);
 						float scaleU = Random.value;
-						float scaleVal = Mathf.Lerp (cloudScaleMin, cloudScaleMax, scaleU);
-						cPos.y = Mathf.Lerp (cloudPosMin.y, cPos.y, scaleU);
+						float scaleVal = Mathf.Lerp (asteroidScaleMin, asteroidScaleMax, scaleU);
+						cPos.y = Mathf.Lerp (asteroidPosMin.y, cPos.y, scaleU);
 						cPos.z = 100 - 90 * scaleU;
-						cloud.transform.position = cPos;
-						cloud.transform.localScale = Vector3.one * scaleVal;
-						cloud.transform.parent = anchor.transform;
-						cloudInstances [i] = cloud;
+						asteroid.transform.position = cPos;
+						asteroid.transform.localScale = Vector3.one * scaleVal;
+						asteroid.transform.parent = anchor.transform;
+						asteroidInstances [i] = asteroid;
 				}
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-				foreach (GameObject cloud in cloudInstances) {
-						float scaleVal = cloud.transform.localScale.x;
-						Vector3 cPos = cloud.transform.position;
-						cPos.x -= scaleVal * Time.deltaTime * cloudSpeedMult;
-						if (cPos.x <= cloudPosMin.x) {
-								cPos.x = cloudPosMax.x;
+				foreach (GameObject asteroid in asteroidInstances) {
+						float scaleVal = asteroid.transform.localScale.x;
+						Vector3 cPos = asteroid.transform.position;
+						cPos.x -= scaleVal * Time.deltaTime * asteroidSpeedMult;
+						if (cPos.x <= asteroidPosMin.x) {
+								cPos.x = asteroidPosMax.x;
 						}
-						cloud.transform.position = cPos;
+						asteroid.transform.position = cPos;
 				}
 		}
 }
