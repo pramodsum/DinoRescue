139133934c038ff53ff011d4c2a1c1d29d14c634
Sumedha <pramods@umich.edu>
2014-10-07 22:50:23 -0400

    Follow Camera finally works

---
+++ Assets/scripts/FollowCam.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FollowCam : MonoBehaviour
+{
+		static public FollowCam S; // a FollowCam Singleton
+	
+		// fields set in the Unity Inspector pane
+		public float easing = 0.05f;
+		public bool ____________________________;
+	
+		// fields set dynamically
+		public GameObject poi;
+		public Vector2 minXY;
+		public float camZ;
+	
+		void Awake ()
+		{
+				S = this;
+				camZ = this.transform.position.z;
+		}
+	
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+				Vector3 destination = poi.transform.position;
+				//Limit the X & Y to minimum values
+				destination.x = Mathf.Max (minXY.x, destination.x);
+				destination.y = Mathf.Max (minXY.y, destination.y);
+				//Interpolate from the current Camera position toward destination
+				destination = Vector3.Lerp (transform.position, destination, easing);
+				// Retain a destination.z of camZ
+				destination.z = camZ;
+				// Set the camera to the destination
+				transform.position = destination;	
+				// Set the orthographicSize of the Camera to keep Ground in view
+				this.camera.orthographicSize = 10;
+		}
+}
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,17 +4,27 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 		private bool isGrounded = true;
+		public Sprite[] sprites;
+		SpriteRenderer spriteRenderer;
+
+		void Start ()
+		{
+				spriteRenderer = renderer as SpriteRenderer;
+		}
 
 		void FixedUpdate ()
 		{
+				spriteRenderer.sprite = sprites [0];
+
 				if (Input.GetKey (KeyCode.LeftArrow)) {
 						transform.position = new Vector3 (transform.position.x - 0.5f, transform.position.y, 0);
 				} else if (Input.GetKey (KeyCode.RightArrow)) {
 						transform.position = new Vector3 (transform.position.x + 0.5f, transform.position.y, 0);
 				}
-				if (Input.GetKey (KeyCode.Space) && isGrounded) {	
+				if (Input.GetKey (KeyCode.Space) && isGrounded) {
+						spriteRenderer.sprite = sprites [1];
 						rigidbody2D.AddForce (Vector3.up * 2.0f, ForceMode2D.Impulse);
-				}
+				} 
 		}
 	
 		void OnCollisionStay (Collision coll)
